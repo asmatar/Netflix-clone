c75deb4f26023637a32367938ac751fe546a352a
asmatar <deruelle.arthur@gmail.com>
2021-04-09 14:08:12 +0200

    toggle menu apparition

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,24 +5,30 @@ import SearchIcon from "@material-ui/icons/Search";
 import React, { useState } from 'react';
 import './Nav.scss';
 const Nav = () => {
-    // On veux detecter la transition au scroll
+    // 1 On veux detecter la transition au scroll
 
-    // 1. creation state local pour mettre nav bar en black si on scroll, par défault la nav n'est pas noir
-    const [navBlack, setNavBlack] = useState(false)
-    // const [toggleMenu, setToggleMenu] = useState(false)
-    // 2. on vérifi lors du scroll vertical. Si on est >100 la nav bar black est a true, sinon false
+    // 1.1 creation state local pour mettre nav bar en black si on scroll, par défault la nav n'est pas noir
+    const [navBlack, setNavBlack] = useState(false);
+    // 2 initialisation du toogle menu, par défaut a false, pour faire apparaitre le menu
+    const [toggleMenu, setToggleMenu] = useState(false);
+    // 2.1 si toggleMenu est a false on affiche pas le menu, et si togglemenu est a true on affiche le menu
+    const handleClick = () => {
+        console.log(toggleMenu)
+        toggleMenu ? setToggleMenu(false) : setToggleMenu(true)
+      }
+    // 1.2 on vérifi lors du scroll vertical. Si on est >100 la nav bar black est a true, sinon false
     const transitionNav = () => {
       window.scrollY > 100 ? setNavBlack(true) : setNavBlack(false)
     }
-    // 3. au scroll on applique le changement
+    // 1.3 au scroll on applique le changement
     useState(() => {
       document.addEventListener("scroll", transitionNav)
     })
     
     return (
-        // si navBlack = true on applique la class "nav--Black"
-        <div className={`nav ${navBlack && "nav--black"}`}>
-           <button className="nav__burger">< MenuIcon /></button>
+        // 1.4 si navBlack = true 2.2 ET QUE le toggl menu =true on applique la class "nav--Black" SINON on applique une class "nav--transparent"
+        <div className={`nav ${navBlack || toggleMenu ? "nav--black" : "nav--transparent"} ${toggleMenu && "show"}`}>
+           <button className="nav__burger" onClick={handleClick}>< MenuIcon /></button>
            <img src="./image/logo.png" alt="Netflix"/>
            <nav className='nav__links'>
                <a href="/" className='nav__link'> 
