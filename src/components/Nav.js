4d9ae55aeaa4095284eebdd18f9f5581a68f31de
asmatar <deruelle.arthur@gmail.com>
2021-04-09 13:15:07 +0200

    fix photo + material ui icon

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,14 @@
+import CardGiftcardIcon from "@material-ui/icons/CardGiftcard";
+import MenuIcon from '@material-ui/icons/Menu';
+import NotificationsIcon from "@material-ui/icons/Notifications";
+import SearchIcon from "@material-ui/icons/Search";
 import React from 'react';
 import './Nav.scss';
-
 const Nav = () => {
+    <SearchIcon/>
     return (
         <div className='nav show nav--black'>
-           <button className="nav__burger">---</button>
+           <button className="nav__burger">< MenuIcon /></button>
            <img src="./image/logo.png" alt="Netflix"/>
            <nav className='nav__links'>
                <a href="/" className='nav__link'> 
@@ -19,16 +23,16 @@ const Nav = () => {
            </nav>
            <div className="nav__actions">
                <a href="/" className='nav_action'>
-                   search
+               <SearchIcon/>
                </a>
                <a href="/" className='nav_action'>
-                   direct
+               direct
                </a>
                <a href="/" className='nav_action'>
-                   gift
+               <CardGiftcardIcon/>
                </a>
                <a href="/" className='nav_action'>
-                   notif
+               <NotificationsIcon/>
                </a>
                <a href="/" className='nav_action'>
                    <img src="./image/avatar.jpg" alt=""/>
