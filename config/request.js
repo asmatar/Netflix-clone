a563c2ee64b3b05499e30d1df8ed16c35212f445
asmatar <deruelle.arthur@gmail.com>
2021-04-09 18:16:51 +0200

    creation du fichier de requete pour API themovie

---
+++ config/request.js
@@ -0,0 +1,18 @@
+// clé api c08ed3943d3afdc4be08c4b190d1fc02
+// https://api.themoviedb.org/3
+
+const API_KEY= "c08ed3943d3afdc4be08c4b190d1fc02"
+const baseURL = "https://api.themoviedb.org/3"
+
+const requests = {
+ fetchTrending: `${baseURL}/trending/all/week?api_key=${API_KEY}`,
+  fetchNetflixOriginals: `${baseURL}/trending/all/week?api_key=${API_KEY}`,
+  fetchTopRated: `${baseURL}/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchActionMovies: `${baseURL}/discover/movie?api_key=${API_KEY}&width_genres=28`,
+  fetchComedyMovies: `${baseURL}/discover/movie?api_key=${API_KEY}&width_genres=35`,
+  fetchHorrorMovies: `${baseURL}/discover/movie?api_key=${API_KEY}&width_genres=27`,
+  fetchRomanceMovies: `${baseURL}/discover/movie?api_key=${API_KEY}&width_genres=10749`,
+  fetchDocumentaries: `${baseURL}/discover/movie?api_key=${API_KEY}&width_genres=99`,
+}
+
+export default requests
