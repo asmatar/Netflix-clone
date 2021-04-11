1b113eec38da92c9db4a45eb70e0a349a7d5796b
asmatar <deruelle.arthur@gmail.com>
2021-04-11 12:00:47 +0200

    setup Route

--- src/App.js
+++ src/App.js
@@ -1,31 +1,40 @@
+import { BrowserRouter as Router, Redirect, Route, Switch } from "react-router-dom";
 import './App.scss';
 import Banner from './components/Banner';
 import Footer from './components/Footer';
 import Nav from './components/Nav';
-import QuickView from './components/QuickView';
 import Row from './components/Row';
+import Video from "./components/Video";
 import requests from './config/request';
 function App() {
   return (
     
     <div className="App">
-      <Nav /> 
-      <Banner />
-      {/*  on doit passer en props le titre de la catégorie et l'URL, les URL sont présentes dans la doc de l'API*/}
-      <Row title='Programmes originaux Netflix' fetchUrl={requests.fetchNetflixOriginals} isPoster ={true}/>
-      <Row title='Tendance actuelle' fetchUrl={requests.fetchTrending}/>
-      <Row title="Les mieux notés" fetchUrl={requests.fetchTopRated} />
-      <Row title="Films d'action" fetchUrl={requests.fetchActionMovies} />
-      <Row title="Films d'horreur" fetchUrl={requests.fetchTrending} />
-      <Row title="Comédies" fetchUrl={requests.fetchTopRated} />
-      <Row title="Documentaires" fetchUrl={requests.fetchActionMovies} />
+      <Router>
+        <Switch>
+          <Route exact path='/'>
+            <Nav /> 
+            <Banner />
+            {/*  on doit passer en props le titre de la catégorie et l'URL, les URL sont présentes dans la doc de l'API*/}
+            <Row title='Programmes originaux Netflix' fetchUrl={requests.fetchNetflixOriginals} isPoster ={true}/>
+            <Row title='Tendance actuelle' fetchUrl={requests.fetchTrending}/>
+            <Row title="Les mieux notés" fetchUrl={requests.fetchTopRated} />
+            <Row title="Films d'action" fetchUrl={requests.fetchActionMovies} />
+            <Row title="Films d'horreur" fetchUrl={requests.fetchTrending} />
+            <Row title="Comédies" fetchUrl={requests.fetchTopRated} />
+            <Row title="Documentaires" fetchUrl={requests.fetchActionMovies} />
 
-      {/* video */}
-      <QuickView />
-      {/* quick view */}
-      
-      {/* footer */}
-      <Footer  />
+            {/* video */}
+            <Footer  />
+          </Route>
+          <Route path='/video/:id'>
+            <Video />
+          </Route>
+          <Route path="*">
+            <Redirect to="/" />
+          </Route>
+      </Switch>
+     </Router>
     </div> 
   );
 }
--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@ import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
+
     <App />
+
   </React.StrictMode>,
   document.getElementById('root')
 );
